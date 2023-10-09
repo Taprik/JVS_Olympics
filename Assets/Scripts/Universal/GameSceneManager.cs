6f54d5ff1ca359afa5b60f238b1a3873c1eb09de
Taprik <taprik@taprik.net>
2023-10-09 17:09:58 +0200

    Add Loading Screen

---
+++ Assets/Scripts/Universal/GameSceneManager.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+using System.Threading.Tasks;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+
+[CreateAssetMenu(fileName = "GameSceneManager", menuName = "Manager/GameSceneManager")]
+public class GameSceneManager : ScriptableObject
+{
+    public SceneName CurrentScene => _currentScene;
+    [SerializeField]
+    SceneName _currentScene;
+
+    public GameObject CurrentSceneObject { get; set; }
+
+    public void Init()
+    {
+        _currentScene = SceneName.MainScene;
+    }
+
+
+    public async void LoadScene(SceneName scene)
+    {
+        if(CurrentScene != SceneName.MainScene)
+            await UnloadScene(CurrentScene, scene != SceneName.MainScene);
+
+        Debug.Log("Unload Finish");
+
+        if(scene != SceneName.MainScene)
+            await LoadScene(scene, LoadSceneMode.Additive);
+
+        Debug.Log("Load Finish");
+
+        _currentScene = scene;
+    }
+
+    public async Task LoadScene(SceneName scene, LoadSceneMode mode)
+    {
+        GameManager.Instance.LoadScreenText.text = "0%";
+        GameManager.Instance.LoadScreenBar.value = 0;
+        GameManager.Instance.LoadScreen.SetActive(true);
+        await Task.Delay(50);
+
+        AsyncOperationHandle<SceneInstance> handle = Addressables.LoadSceneAsync(scene.ToString(), mode);
+        handle.Completed += async (handle) =>
+        {
+            GameManager.Instance.LoadScreenText.text = "100%";
+            GameManager.Instance.LoadScreenBar.DOValue(1, 0.1f);
+            await Task.Delay(500);
+            GameManager.Instance.LoadScreen.SetActive(false);
+            GameManager.Instance.MainSceneObject.SetActive(false);
+        };
+
+        while (!handle.IsDone)
+        {
+            Tween tween = GameManager.Instance.LoadScreenBar.DOValue(handle.PercentComplete, 0.1f);
+
+            GameManager.Instance.LoadScreenText.text = Mathf.RoundToInt(handle.PercentComplete * 100f).ToString() + "%";
+
+            await tween.AsyncWaitForCompletion();
+        }
+
+    }
+
+    public async Task UnloadScene(SceneName scene, bool isGoingToLoad = false)
+    {
+        GameManager.Instance.LoadScreenText.text = "0%";
+        GameManager.Instance.LoadScreenBar.value = 0;
+        GameManager.Instance.LoadScreen.SetActive(true);
+        await Task.Delay(50);
+
+        AsyncOperation operation = SceneManager.UnloadSceneAsync(scene.ToString());
+        operation.completed += async (operation) =>
+        {
+            if(!isGoingToLoad)
+            {
+                GameManager.Instance.LoadScreenText.text = "100%";
+                GameManager.Instance.LoadScreenBar.DOValue(1, 0.1f);
+                await Task.Delay(500);
+            }
+            else if(CurrentSceneObject != null)
+            {
+                CurrentSceneObject.SetActive(false);
+                CurrentSceneObject = null;
+            }
+            GameManager.Instance.LoadScreen.SetActive(false);
+            GameManager.Instance.MainSceneObject.SetActive(true);
+        };
+
+        while (!operation.isDone)
+        {
+            Tween tween = GameManager.Instance.LoadScreenBar.DOValue(operation.progress, 0.1f);
+
+            GameManager.Instance.LoadScreenText.text = Mathf.RoundToInt(Mathf.Clamp01(operation.progress / 0.9f) * 100f).ToString() + "%";
+
+            await tween.AsyncWaitForCompletion();
+        }
+
+    }
+
+}
+
+public enum SceneName
+{
+    MainScene,
+    Game,
+    Game2
+}
