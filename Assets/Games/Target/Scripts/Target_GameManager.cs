55742e22de4884f5ae5764be5a684cdfd8e9ee28
PtiteMadeleine <115978909+PtiteMadeleine@users.noreply.github.com>
2024-09-23 16:46:41 +0200

    Add Manager Scripts

---
+++ Assets/Games/Target/Scripts/Target_GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Target
+{
+    public class Target_GameManager : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/Games/Target/Scripts/Target_SceneObject.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Target
+{
+    public class Target_SceneObject : GameSceneObject
+    {
+        public override void OnNameReceive(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void PageDown()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void PageUp()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Task Replay()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
